244ee4e8bbf3038dd94db6f0a9d1ca9ac8edff89
David Casalod Ezquerro <david.casalod@atos.net>
2023-02-03 09:30:44 +0100

    cambios 2.4

--- Client_dapp_bd4opem/server/src/connection.ts
+++ Client_dapp_bd4opem/server/src/connection.ts
@@ -5,62 +5,81 @@
  */
 
 import * as grpc from '@grpc/grpc-js';
-import { connect, Gateway, Identity, Signer, signers } from '@hyperledger/fabric-gateway';
+import {  Identity, Signer, signers } from '@hyperledger/fabric-gateway';
 import * as crypto from 'crypto';
-import * as fs from 'fs';
+import { promises as fs } from 'fs';
 import * as path from 'path';
-import { CLIENT_CERT_PATH, GATEWAY_ENDPOINT, HOST_ALIAS, MSP_ID, PRIVATE_KEY_PATH, TLS_CERT_PATH } from './config';
+
+
+// export async function newGrpcConnection(): Promise<grpc.Client> {
+//     if (TLS_CERT_PATH) {
+//         const tlsRootCert = await fs.promises.readFile(TLS_CERT_PATH);
+//         const tlsCredentials = grpc.credentials.createSsl(tlsRootCert);
+//         return new grpc.Client(GATEWAY_ENDPOINT, tlsCredentials, newGrpcClientOptions());
+//     }
+
+//     return new grpc.Client(GATEWAY_ENDPOINT, grpc.ChannelCredentials.createInsecure());
+// }
+
+const mspId = envOrDefault('MSP_ID', 'Org1MSP');
+
+// Path to crypto materials.
+const cryptoPath = envOrDefault('CRYPTO_PATH', path.resolve(__dirname, '..', '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com'));
+
+// Path to user private key directory.
+const keyDirectoryPath = envOrDefault('KEY_DIRECTORY_PATH', path.resolve(cryptoPath, 'users', 'User1@org1.example.com', 'msp', 'keystore'));
+
+// Path to user certificate.
+const certPath = envOrDefault('CERT_PATH', path.resolve(cryptoPath, 'users', 'User1@org1.example.com', 'msp', 'signcerts', 'cert.pem'));
+
+// Path to peer tls certificate.
+const tlsCertPath = envOrDefault('TLS_CERT_PATH', path.resolve(cryptoPath, 'peers', 'peer0.org1.example.com', 'tls', 'ca.crt'));
+
+// Gateway peer endpoint.
+const peerEndpoint = envOrDefault('PEER_ENDPOINT', 'localhost:7051');
+
+// Gateway peer SSL host name override.
+const peerHostAlias = envOrDefault('PEER_HOST_ALIAS', 'peer0.org1.example.com');
+
+/**
+ * envOrDefault() will return the value of an environment variable, or a default value if the variable is undefined.
+ */
+
+
 
 export async function newGrpcConnection(): Promise<grpc.Client> {
-    if (TLS_CERT_PATH) {
-        const tlsRootCert = await fs.promises.readFile(TLS_CERT_PATH);
-        const tlsCredentials = grpc.credentials.createSsl(tlsRootCert);
-        return new grpc.Client(GATEWAY_ENDPOINT, tlsCredentials, newGrpcClientOptions());
-    }
-
-    return new grpc.Client(GATEWAY_ENDPOINT, grpc.ChannelCredentials.createInsecure());
-}
-
-function newGrpcClientOptions(): grpc.ClientOptions {
-    const result: grpc.ClientOptions = {};
-    if (HOST_ALIAS) {
-        result['grpc.ssl_target_name_override'] = HOST_ALIAS; // Only required if server TLS cert does not match the endpoint address we use
-    }
-    return result;
-}
-
-export async function newGatewayConnection(client: grpc.Client): Promise<Gateway> {
-    return connect({
-        client,
-        identity: await newIdentity(),
-        signer: await newSigner(),
-        // Default timeouts for different gRPC calls
-        evaluateOptions: () => {
-            return { deadline: Date.now() + 5000 }; // 5 seconds
-        },
-        endorseOptions: () => {
-            return { deadline: Date.now() + 15000 }; // 15 seconds
-        },
-        submitOptions: () => {
-            return { deadline: Date.now() + 5000 }; // 5 seconds
-        },
-        commitStatusOptions: () => {
-            return { deadline: Date.now() + 60000 }; // 1 minute
-        },
+    const tlsRootCert = await fs.readFile(tlsCertPath);
+    const tlsCredentials = grpc.credentials.createSsl(tlsRootCert);
+    return new grpc.Client(peerEndpoint, tlsCredentials, {
+        'grpc.ssl_target_name_override': peerHostAlias,
     });
 }
 
-async function newIdentity(): Promise<Identity> {
-    const certPath = path.resolve(CLIENT_CERT_PATH);
-    const credentials = await fs.promises.readFile(certPath);
-
-    return { mspId: MSP_ID, credentials };
+export async function newIdentity(): Promise<Identity> {
+    const credentials = await fs.readFile(certPath);
+    return { mspId, credentials };
 }
 
-async function newSigner(): Promise<Signer> {
-    const keyPath = path.resolve(PRIVATE_KEY_PATH);
-    const privateKeyPem = await fs.promises.readFile(keyPath);
+export async function newSigner(): Promise<Signer> {
+    const files = await fs.readdir(keyDirectoryPath);
+    const keyPath = path.resolve(keyDirectoryPath, files[0]);
+    const privateKeyPem = await fs.readFile(keyPath);
     const privateKey = crypto.createPrivateKey(privateKeyPem);
-
     return signers.newPrivateKeySigner(privateKey);
+}
+
+function envOrDefault(key: string, defaultValue: string): string {
+    return process.env[key] || defaultValue;
+}
+/**
+ * displayInputParameters() will print the global scope parameters used by the main driver routine.
+ */
+export async function displayInputParameters(): Promise<void> {
+    console.log(`mspId:             ${mspId}`);
+    console.log(`cryptoPath:        ${cryptoPath}`);
+    console.log(`keyDirectoryPath:  ${keyDirectoryPath}`);
+    console.log(`certPath:          ${certPath}`);
+    console.log(`tlsCertPath:       ${tlsCertPath}`);
+    console.log(`peerEndpoint:      ${peerEndpoint}`);
+    console.log(`peerHostAlias:     ${peerHostAlias}`);
 }
